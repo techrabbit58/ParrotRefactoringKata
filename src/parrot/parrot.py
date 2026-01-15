6b793a456f6582ee1ec46bd44aa4cd8766665955
techrabbit58 <dzb@pruefbit.de>
2026-01-15 17:47:30 +0100

    r apply static type checking

--- src/parrot/parrot.py
+++ src/parrot/parrot.py
@@ -11,37 +11,37 @@ class ParrotType(Enum):
 class Parrot(ABC):
 
     @abstractmethod
-    def speed(self):
+    def speed(self) -> str:
         raise NotImplementedError()
 
     @abstractmethod
-    def cry(self):
+    def cry(self) -> str:
         raise NotImplementedError()
 
     @staticmethod
-    def _load_factor():
+    def _load_factor() -> float:
         return 9.0
 
     @staticmethod
-    def _base_speed():
+    def _base_speed() -> float:
         return 12.0
 
 
 class EuropeanParrot(Parrot):
 
-    def speed(self):
+    def speed(self) -> float:
         return self._base_speed()
 
-    def cry(self):
+    def cry(self) -> str:
         return "Sqoork!"
 
 
 class AfricanParrot(Parrot):
-    def __init__(self, number_of_coconuts: int) -> None:
+    def __init__(self, number_of_coconuts: float) -> None:
         self._number_of_coconuts = number_of_coconuts
 
-    def speed(self):
-        return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+    def speed(self) -> float:
+        return max(0., self._base_speed() - self._load_factor() * self._number_of_coconuts)
 
     def cry(self) -> str:
         return "Sqaark!"
@@ -52,13 +52,13 @@ class NorwegianBlue(Parrot):
         self._voltage = voltage
         self._nailed = nailed
 
-    def speed(self) -> None:
+    def speed(self) -> float:
         return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
 
-    def cry(self):
+    def cry(self) -> str:
         return "Bzzzzzz" if self._voltage > 0 else "..."
 
-    def _compute_base_speed_for_voltage(self, voltage):
+    def _compute_base_speed_for_voltage(self, voltage) -> float:
         return min([24.0, voltage * self._base_speed()])
 
 
